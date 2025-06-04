f193770fc9b8f989577b505fcd7729089a324c65
tech temple <sachinpathe230498@gmail.com>
2025-06-04 13:25:17 +0530

    array methods

---
+++ javascript/array_methods/prob5.js
@@ -0,0 +1,29 @@
+// slice
+
+// let arr = [1,2,3,4]
+
+// let new_arr = arr.slice(1,4)
+
+// console.log(new_arr);
+
+
+
+// slice method is used for  getting a part of array
+// 1st parameter starting index
+// 2nd parameter is ending index 
+//it return new array
+
+let arr = [1, 2, 3, 4];
+
+let abc = arr.slice(2,5)
+
+console.log(abc)
+
+
+
+
+
+
+
+
+
---
+++ javascript/array_methods/prob6.js
@@ -0,0 +1,22 @@
+// sort method
+
+let arr = [7,9,8,5,4,1,2,11,44,77,45]
+
+
+
+let sorted = arr.sort(function(a,b){
+    return b-a
+})
+
+console.log(sorted)
+
+
+// let str = ["hanuman", "amol", "harsh", "vaibhav", "sachin"]
+
+// arr.sort()
+
+// str.sort()
+
+// console.log(str)
+
+// // console.log(arr)
